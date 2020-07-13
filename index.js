60dcf40b56b31028173a4da22594d8c9dc429088
Benjamin Sampica <Benjamin.Sampica@polkcountyiowa.gov>
2020-07-12 21:38:47 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+﻿const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const chosenClass = core.getInput('class');
+    console.log(`Hello ${chosenClass}!`);
+} catch (error) {
+    core.setFailed(error.message);
+}
