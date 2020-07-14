5d0c2b8162f8abfa98693ee1c71bd3350a92f5fa
Benjamin Sampica <Benjamin.Sampica@polkcountyiowa.gov>
2020-07-13 21:38:23 -0500

    update index

--- roll/index.js
+++ roll/index.js
@@ -34,7 +34,7 @@ const getDatabaseValue = (databaseText, textToFind) => {
 
 const updateClassTableText = () => {
     readMeText += "|Classes|Count|\n" +
-        "|-|-|" +
+        "|-|-|\n" +
         "|Warrior|" + getDatabaseValue(classDatabaseText, "warrior") + "\n" +
         "|Cleric|" + getDatabaseValue(classDatabaseText, "cleric") + "\n" +
         "|Rogue|" + getDatabaseValue(classDatabaseText, "rogue") + "\n" +
@@ -51,10 +51,12 @@ const updateRollTableText = () => {
     }
 }
 
-const setDatabaseValue = (databaseText, textToFind) => {
+const setDatabaseValue = (databaseText, textToFind, newValue) => {
     let textArray = splitDatabaseText(databaseText, textToFind);
-    const newText = textArray[0] + "|" + newClassValue;
+    const newText = textArray[0] + "|" + newValue;
     const textToReplace = textArray[0] + textArray[1];
+    console.log(textToReplace);
+    console.log(newValue);
     databaseText.replace(textToReplace, newText);
 }
 
@@ -69,17 +71,20 @@ const usersClass = getChosenClass();
 const newClassValue = getDatabaseValue(classDatabaseText, usersClass);
 const roll = getRoll(usersClass);
 
-setDatabaseValue(classDatabaseText, usersClass);
-setDatabaseValue(rollDatabaseText, roll.toString());
+console.log(classDatabaseText);
+setDatabaseValue(classDatabaseText, usersClass, newClassValue);
+setDatabaseValue(rollDatabaseText, roll.toString(), roll.toString());
+console.log(classDatabaseText);
 
 let readMeText =
     "# ROLL FOR INITIATIVE\n" +
     "### CHOOSE YOUR CLASS\n" +
-    "[Warrior | +1 To Roll](https://github.com/benjaminsampica/benjaminsampica/issues/new?title=roll%7Cwarrior&body=Just+click+%27Submit+new+issue%27.)\n" +
-    "[Cleric | +1 To Roll](https://github.com/benjaminsampica/benjaminsampica/issues/new?title=roll%7Ccleric&body=Just+click+%27Submit+new+issue%27.)\n" +
-    "[Rogue | +3 To Roll](https://github.com/benjaminsampica/benjaminsampica/issues/new?title=roll%7Crogue&body=Just+click+%27Submit+new+issue%27.)\n" +
-    "[Wizard | -1 To Roll](https://github.com/benjaminsampica/benjaminsampica/issues/new?title=roll%7Cwizard&body=Just+click+%27Submit+new+issue%27.)\n" +
-    "### LAST ROLL BY\n";
+    "\n[Warrior | +1 To Roll](https://github.com/benjaminsampica/benjaminsampica/issues/new?title=roll%7Cwarrior&body=Just+click+%27Submit+new+issue%27.)\n" +
+    "\n[Cleric | +1 To Roll](https://github.com/benjaminsampica/benjaminsampica/issues/new?title=roll%7Ccleric&body=Just+click+%27Submit+new+issue%27.)\n" +
+    "\n[Rogue | +3 To Roll](https://github.com/benjaminsampica/benjaminsampica/issues/new?title=roll%7Crogue&body=Just+click+%27Submit+new+issue%27.)\n" +
+    "\n[Wizard | -1 To Roll](https://github.com/benjaminsampica/benjaminsampica/issues/new?title=roll%7Cwizard&body=Just+click+%27Submit+new+issue%27.)\n" +
+    "### LAST ROLL BY\n" +
+    core.getInput('user');
 
 updateClassTableText();
 updateRollTableText();
