a69a8a72ca594aa35e547e46253454661d53a8b6
Benjamin Sampica <Benjamin.Sampica@polkcountyiowa.gov>
2020-07-13 19:01:50 -0500

    Update index.js

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ### ROLL FOR INITIATIVE
 
-
+[Warrior | +1 To Roll](https://github.com/benjaminsampica/benjaminsampica/issues/new?title=roll%7Cwarrior)
+[Cleric | +1 To Roll](https://github.com/benjaminsampica/benjaminsampica/issues/new?title=roll%7Ccleric)
+[Rogue | +3 To Roll](https://github.com/benjaminsampica/benjaminsampica/issues/new?title=roll%7Crogue)
+[Wizard | -1 To Roll](https://github.com/benjaminsampica/benjaminsampica/issues/new?title=roll%7Cwizard)
 
 ![visitors](https://visitor-badge.glitch.me/badge?page_id=benjaminsampica)
--- roll/index.js
+++ roll/index.js
@@ -1,9 +1,20 @@
-﻿﻿const core = require('@actions/core');
-const github = require('@actions/github');
+﻿const core = require('@actions/core');
 
-try {
-    const title = core.getInput('title');
-    console.log(`Hello ${title}!`);
-} catch (error) {
-    core.setFailed(error.message);
-}
+let getRandomNumber = () => Math.floor((Math.random() * 20) + 1);
+let getClassInitiativeBonus = chosenClass => {
+    if(chosenClass === "warrior") return 1;
+    if(chosenClass === "cleric") return 1;
+    if(chosenClass === "rogue") return 3;
+    if(chosenClass === "wizard") return -1;
+};
+
+const title = core.getInput('title') || "roll|warrior";
+const titleArray = title.split(`|`);
+
+let roll = getRandomNumber() + getClassInitiativeBonus(titleArray[1]);
+console.log(roll);
+
+return roll;
+
+
+
